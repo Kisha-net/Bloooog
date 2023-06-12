1a08f542b2323ee81b347b6a96f3b970c2e1162e
kisha-net <antoinetteakinyi7@gmail.com>
2023-06-12 14:38:47 +0300

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body> 
+    <article>
+
+        <h1>My First Post</h1>
+
+        <p> blablaLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id dui at mi consequat placerat. Nullam semper bibendum ante, vitae viverra felis consectetur id. Fusce ultricies consequat nisi, vel finibus velit eleifend ut.</p>
+    </article>
+
+    <a href="/">Go back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body> 
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p> blablaLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id dui at mi consequat placerat. Nullam semper bibendum ante, vitae viverra felis consectetur id. Fusce ultricies consequat nisi, vel finibus velit eleifend ut.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p> blablaLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id dui at mi consequat placerat. Nullam semper bibendum ante, vitae viverra felis consectetur id. Fusce ultricies consequat nisi, vel finibus velit eleifend ut.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p> blablaLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id dui at mi consequat placerat. Nullam semper bibendum ante, vitae viverra felis consectetur id. Fusce ultricies consequat nisi, vel finibus velit eleifend ut.</p>
+    </article>
+</body>
